2808486fa7f9bd4aa473d32d5a3165beca9e95f3
ph140 <petterh140@gmail.com>
2021-04-19 14:58:16 +0200

    to dimensjoner

--- rocket.py
+++ rocket.py
@@ -1,5 +1,6 @@
 # Importerer nødvendige bibliotek
 # from matplotlib import pyplot as plt
+import math
 
 # Konstanter
 gravitational_constant = 6.67408E-11  # m^3 kg^-1 s^-2
@@ -19,16 +20,18 @@ field_strength = []
 
 
 class Rocket():
-    def __init__(self, altitude, velocity, acceleration, mass,
-                 methane, LOX, fuel):
-        self.altitude = altitude
-        self.velocity = velocity
-        self.acceleration = acceleration  # Akselerasjon
+    def __init__(self, position_x, position_y, velocity_x, velocity_y,
+                 acceleration_x, acceleration_y, mass, methane, LOX, fuel):
+        self.position_x = position_x  # Posisjon/m
+        self.position_y = position_y  # Posisjon/m
+        self.velocity_x = velocity_x  # Hastighet/ m/s
+        self.velocity_y = velocity_y  # Hastighet/ m/s
+        self.acceleration_x = acceleration_x  # Akselerasjon m/s^2
+        self.acceleration_y = acceleration_y  # Akselerasjon m/s^2
         self.mass = mass  # Masse
-        self.methane = methane  # Flytende metan
-        self.LOX = LOX  # Flytende oksygen
         self.fuel = fuel
         self.throttle = 0
+        self.pitch = math.pi/2
 
     def acceleration(cls):
         return (gravity(cls.altitude) + thrust())/cls.mass
@@ -37,7 +40,20 @@ class Rocket():
         return cls.throttle * 2.21E6  # Kraft av motorene / Newton
 
 
-raptor = Rocket(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
+class Engine():
+    def __init__(self, angle):
+        self.throttle = 0
+        self.angle = angle
+
+    def thrust(cls):
+        return cls.throttle * 2.21E6  # Kraft av motorene / Newton
+
+
+raptor1 = Engine(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
+raptor2 = Engine(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
+raptor3 = Engine(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
+
+hogstad_rocket = Rocket(0, earth_radius, 0, 0, 0, 0, rocket_mass)
 
 
 # Funksjoner
@@ -55,7 +71,8 @@ def thrust():
     return
 
 
-print(raptor.mass)
+print(math.sin(0))
+
 
 # for i in range(1000000):
 #     altitude.append(i)
