69d469367bf431e24a2eeae4859b8b3c4c266f56
ph140 <petterh140@gmail.com>
2021-04-19 17:43:34 +0200

    Posisjon, hastighet og akselerasjon med gravitasjon

--- rocket.py
+++ rocket.py
@@ -20,18 +20,18 @@ field_strength = []
 
 
 class Rocket():
-    def __init__(self, position_x, position_y, velocity_x, velocity_y,
-                 acceleration_x, acceleration_y, mass, methane, LOX, fuel):
-        self.position_x = position_x  # Posisjon/m
-        self.position_y = position_y  # Posisjon/m
-        self.velocity_x = velocity_x  # Hastighet/ m/s
-        self.velocity_y = velocity_y  # Hastighet/ m/s
-        self.acceleration_x = acceleration_x  # Akselerasjon m/s^2
-        self.acceleration_y = acceleration_y  # Akselerasjon m/s^2
-        self.mass = mass  # Masse
-        self.fuel = fuel
-        self.throttle = 0
-        self.pitch = math.pi/2
+    def __init__(self, position, velocity, acceleration, mass, methane,
+                 LOX, fuel):
+        self.position = position  # Posisjon/m
+        self.velocity = velocity  # Hastighet/ m/s
+        self.acceleration = acceleration  # Akselerasjon m/s^2
+        self.mass = mass  # Masse/kg
+        self.fuel = fuel  # Masse/kg
+        self.pitch = math.pi/2  # Vinkel/rad
+
+    @property
+    def altitude(cls):
+        return math.sqrt(cls.position[0]+cls.position[1])
 
     def acceleration(cls):
         return (gravity(cls.altitude) + thrust())/cls.mass
@@ -49,11 +49,13 @@ class Engine():
         return cls.throttle * 2.21E6  # Kraft av motorene / Newton
 
 
-raptor1 = Engine(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
-raptor2 = Engine(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
-raptor3 = Engine(0, 0, 0, rocket_mass, 0, 0, fuel_mass)
+raptor1 = Engine()
+raptor2 = Engine()
+raptor3 = Engine()
 
-hogstad_rocket = Rocket(0, earth_radius, 0, 0, 0, 0, rocket_mass)
+
+hogstad_rocket = Rocket([0, earth_radius], [0, 0], [
+                        0, 0], rocket_mass, fuel_mass,)
 
 
 # Funksjoner
@@ -71,8 +73,7 @@ def thrust():
     return
 
 
-print(math.sin(0))
-
+print(hogstad_rocket.altitude)
 
 # for i in range(1000000):
 #     altitude.append(i)
